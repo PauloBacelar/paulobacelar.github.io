35206aaebfa816844b1fa1332b8f30b943b01db1
Paulo Guilherme Bacelar Andrade <p270114@dac.unicamp.br>
2023-09-03 19:07:25 -0300

    Fixed FormSubmit and removed mailto

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-// Submit form
- 
+// Copy to clipboard
+const copyToClipboard = () => {
+  navigator.clipboard.writeText('paulobacelar.dev@gmail.com');
+  const copiedBtn = document.getElementById('copied');
+  copiedBtn.style.color = '#e5e5e6da';
+};
 
-/* -----------------------------------------
-  Have focus outline only for keyboard users 
- ---------------------------------------- */
-
+// Handle keyboard browsing
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing');
 
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
+    window.removeEventListener('keydown', handleFirstTab);
+    window.addEventListener('mousedown', handleMouseDownOnce);
   }
-
-}
+};
 
 const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+  document.body.classList.remove('user-is-tabbing');
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
+  window.removeEventListener('mousedown', handleMouseDownOnce);
+  window.addEventListener('keydown', handleFirstTab);
+};
 
-window.addEventListener('keydown', handleFirstTab)
+window.addEventListener('keydown', handleFirstTab);
 
-const backToTopButton = document.querySelector(".back-to-top");
+const backToTopButton = document.querySelector('.back-to-top');
 let isBackToTopRendered = false;
 
 let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.visibility = isBackToTopRendered ? 'visible' : 'hidden';
   backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
   backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
+    ? 'scale(1)'
+    : 'scale(0)';
 };
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY > 700) {
     isBackToTopRendered = true;
     alterStyles(isBackToTopRendered);
